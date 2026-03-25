cbc9f7ad483788494af8bbf1fc750dae38f7d06b
Jeffrey Smith <jeffrey.smith@acceldata.io>
2026-03-25 12:09:04 -0400

    Some small fixes

--- build_deb.sh
+++ build_deb.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 PYTHON_VERSION=${PYTHON_VERSION:-3.11}
-OUTPUT_DIR=${OUTPUT_DIR:-$(pwd)}
+OUTPUT_DIR=${OUTPUT_DIR:-$(pwd)/dist}
 # Generate debian/control from template
 sed "s/@PYTHON@/python${PYTHON_VERSION}/g" debian/control.in >debian/control
 
-PYTHON_VERSION=${PYTHON_VERSION} dpkg-buildpackage -us -uc -b \
-  --builddir=${OUTPUT_DIR}
+PYTHON_VERSION=${PYTHON_VERSION} dpkg-buildpackage -us -uc -b
+
+mv ../python${PYTHON_VERSION}-systemd-dbus_*.deb ${OUTPUT_DIR}/ 2>/dev/null || true
 
 # Cleanup generated files
 rm -f debian/control
