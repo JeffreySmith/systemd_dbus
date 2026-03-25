f407f47f47d97a3a64ee07cfc2439a41492aaadc
Jeffrey Smith <jeffrey.smith@acceldata.io>
2026-03-25 12:20:16 -0400

    Create output dir if it doesn't exist

--- build_deb.sh
+++ build_deb.sh
@@ -4,6 +4,10 @@ OUTPUT_DIR=${OUTPUT_DIR:-$(pwd)/dist}
 # Generate debian/control from template
 sed "s/@PYTHON@/python${PYTHON_VERSION}/g" debian/control.in >debian/control
 
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
+
 PYTHON_VERSION=${PYTHON_VERSION} dpkg-buildpackage -us -uc -b
 
 mv ../python${PYTHON_VERSION}-systemd-dbus_*.deb ${OUTPUT_DIR}/ 2>/dev/null || true
